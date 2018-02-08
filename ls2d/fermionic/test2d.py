400fb6ca712df0af2aeb06b5e4e167e19cc27e23
zhendongli <zhendongli2008@gmail.com>
2018-02-07 23:52:40 -0800

    test2d

--- ls2d/fermionic/test2d.py
+++ ls2d/fermionic/test2d.py
@@ -13,15 +13,11 @@ t2d = exact2d.genT2d(n,mass)
 t2d = t2d.reshape((n*n,n*n))
 tinv = scipy.linalg.inv(t2d)
 tinv = tinv.reshape((n,n,n,n))
-
 #plt.matshow(tinv[m,m])
 #plt.show()
 
 def exactFun(x):
    return scipy.special.k0(mass*x)/(2*numpy.pi)
-
-print t2d.shape
-      
 x = numpy.linspace(0.01,m*numpy.sqrt(2),1000)
 y = exactFun(x)
 
@@ -30,9 +26,25 @@ plt.plot(tinv[m,m][m,range(m,n)],'go',label='horizontal')
 plt.plot(tinv[m,m][range(m,n),m],'bo',label='vertical')
 xd = numpy.arange(m+1)*numpy.sqrt(2)
 plt.plot(xd,tinv[m,m][range(m,n),range(m,n)],'ro',label='diagonal')
+
+#
+# Numerical test
+#
+import genPEPO
+abond = 30
+npepo = genPEPO.genNPEPO(n,mass**2,iprt=0,auxbond=abond)
+numpy.random.seed(0)
+print '\nRandomCheck:'
+for i in range(10):
+   rint = numpy.random.randint(1,high=n-1,size=4)
+   palst = [(rint[0],rint[1])]
+   pblst = [(rint[2],rint[3])]
+   v0 = tinv[palst[0]][pblst[0]]
+   for abond in [10,20,30]:
+      v1 = genPEPO.pepo2cpeps(npepo,palst,pblst,auxbond=abond)[0,0]
+      print i,palst,pblst,'v0=',v0,'Dcut=',abond,'v1=',v1,'diff=',v0-v1
+   print
+exit()
+
 plt.legend()
 plt.show()
-
-print t2d
-print 'detM=',scipy.linalg.det(t2d)
-exit()
