acd2e6a6929b398628c960f0b76764f2e02e4259
zhendongli <zhendongli2008@gmail.com>
2018-07-03 17:51:38 -0700

    gen3d prepare

--- ls3d/gen3d.py
+++ ls3d/gen3d.py
@@ -6,20 +6,72 @@ import scipy.linalg
 import genSite
 from latticesimulation.ls2d import contraction2d
 
+# C1-E1-C2
+# |  |  |
+# E2-I0-E3
+# |  |  | 
+# C3-E4-C4
+def gen_c1(info):
+   return numpy.ones((1,1,1,1))
+def gen_c2(info):
+   return numpy.ones((1,1,1,1))
+def gen_c3(info):
+   return numpy.ones((1,1,1,1))
+def gen_c4(info):
+   return numpy.ones((1,1,1,1))
+def gen_e1(info):
+   return numpy.ones((1,1,1,1))
+def gen_e2(info):
+   return numpy.ones((1,1,1,1))
+def gen_e3(info):
+   return numpy.ones((1,1,1,1))
+def gen_e4(info):
+   return numpy.ones((1,1,1,1))
+def gen_i0(info):
+   return numpy.ones((1,1,1,1))
+
 def initialization(n,mass2=1.0,iprt=0,auxbond=20,guess=None):
    print '\n[gen2d.initialization] n=',n,' mass2=',mass2
    # Interior
-   d = 4 
-   lam = 4.0+mass2
-   tint = genSite.genZSite2D(lam,0)
-   zpeps = numpy.empty((n,n), dtype=numpy.object)
+   n2 = n**2
+   lam = 6.0+mass2
+   tint = genSite.genZSite3D(lam,0)
+   zpeps = numpy.empty((n2,n2), dtype=numpy.object)
+   # Prepare
+   info = [n,tint]
+   c1 = gen_c1(info)
+   c2 = gen_c2(info)
+   c3 = gen_c3(info)
+   c4 = gen_c4(info)
+   e1 = gen_e1(info)
+   e2 = gen_e2(info)
+   e3 = gen_e3(info)
+   e4 = gen_e4(info)
+   i0 = gen_i0(info)
    # Corners
+   zpeps[:n,:n] = c1.copy()
+   zpeps[:n,n2-n:n2] = c2.copy()
+   zpeps[n2-n:n2,:n] = c3.copy()
+   zpeps[n2-n:n2,n2-n:n2] = c4.copy()
+   # Edges
+   for j in range(1,n-1):
+      zpeps[:n,j*n:(j+1)*n] = e1.copy()
+   for j in range(1,n-1):
+      zpeps[n2-n:n2,j*n:(j+1)*n] = e4.copy()
+   for i in range(1,n-1):
+      zpeps[i*n:(i+1)*n,:n] = e2.copy()
+   for i in range(1,n-1):
+      zpeps[i*n:(i+1)*n,n2-n:n2] = e3.copy()
+   # Inner
+   for i in range(1,n-1):
+      for j in range(1,n-1):
+	 zpeps[i*n:(i+1)*n,j*n:(j+1)*n] = i0.copy()
    # Compute scaling factor
    scale,z = contraction2d.binarySearch(zpeps,auxbond,iprt=iprt,guess=guess)
    # Local terms
-   local2  = scale*genSite.genZSite2D(lam,1)
-   local1a = scale*genSite.genZSite2D(lam,2)
-   local1b = scale*genSite.genZSite2D(lam,3)
+   local2  = scale*genSite.genZSite3D(lam,1)
+   local1a = scale*genSite.genZSite3D(lam,2)
+   local1b = scale*genSite.genZSite3D(lam,3)
    zpeps = scale*zpeps
    return scale,zpeps,local2,local1a,local1b
 
